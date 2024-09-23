3f6a29aff1e041c983abe3bf2fad52d5e3158837
KholisEnKa22 <enka.run2206@gmail.com>
2024-09-23 19:13:31 +0700

    ui button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
     <div class="row">
+        <div class="col-lg-12 mb-4 order-0">
+            <div class="card">
+                <div class="d-flex align-items-end row">
+                    <div class="col-sm-7">
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Welcome Back Bruuhh! 🎉</h5>
+                            <p class="mb-4">
+                                Selamat datang kembali di website kami <span class="fw-bold">WA BLAST</span>. Kami juga
+                                menyediakan beberapa fitur yang dapat anda gunakan, jika anda berminat silahkan hubungi
+                                developer kami.
+                            </p>
+                            <a href="https://wa.me/6281554850403" class="btn btn-sm btn-outline-primary">Contact me</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 text-center text-sm-left">
+                        <div class="card-body pb-0 px-0 px-md-4">
+                            <img src="{{ asset('template/templateAdmin/assets/img/illustrations/man-with-laptop-light.png') }}"
+                                height="170" alt="View Badge User"
+                                data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                data-app-light-img="illustrations/man-with-laptop-light.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-lg-3 col-md-6 col-12 mb-4">
             <div class="card">
                 <div class="card-body">
